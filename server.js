69b451273018a849650fdf72a5c1e92ab31011e6
rupert-ong <rupert.ong@gmail.com>
2018-01-09 22:35:25 -0500

    Fix typo

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = require('./app');
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 server.listen(port, () => {
-  console.log(`Server is running on port ${3000}`)
+  console.log(`Server is running on port ${port}`)
 });
