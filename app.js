e1fdfced1a174e81d2048b56184e9019e8258a9b
rupert-ong <rupert.ong@gmail.com>
2018-01-11 21:49:54 -0500

    Add error handling and routes

--- app.js
+++ app.js
@@ -7,7 +7,24 @@ const orderRoutes = require('./api/routes/orders');
 
 app.use(morgan('dev'));
 
+// API route handling
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+// 404 error middleware
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status =404;
+  next(error);
+});
+
+// Error route handling
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    error: {
+      message: error.message
+    }
+  })
+});
 
 module.exports = app;
