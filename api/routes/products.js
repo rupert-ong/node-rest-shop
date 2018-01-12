71ab547002afdba144286aa0f1f2d80529ebc5b4
rupert-ong <rupert.ong@gmail.com>
2018-01-11 21:11:05 -0500

    Add products API routes

---
+++ api/routes/products.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Handling GET requests to /products'
+  });
+});
+
+router.post('/', (req, res, next) => {
+  res.status(201).json({
+    message: 'Handling POST requests to /products'
+  });
+});
+
+router.get('/:productId', (req, res, next) => {
+  const id = req.params.productId;
+  const payload = id === 'special' ? {
+    message: 'Special ID discovered',
+    id: id
+  } : { message: `Handling GET request to /products/${id}` };
+
+  res.status(200).json(payload);
+});
+
+router.patch('/:productId', (req, res, next) => {
+  const id = req.params.productId;
+  res.status(200).json({
+    message: `Handling PATCH request to /products/${id}`
+  });
+});
+
+router.delete('/:productId', (req, res, next) => {
+  const id = req.params.productId;
+  res.status(200).json({
+    message: `Handling DELETE request to /products/${id}`
+  });
+});
+
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const productRoutes = require('./api/routes/products');
+
+app.use('/products', productRoutes);
 
 app.use((req, res, next) => {
   res.status(200).json({
