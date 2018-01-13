dad522ac6cd625bc150a0b98e163924a98d45d0c
rupert-ong <rupert.ong@gmail.com>
2018-01-12 21:01:04 -0500

    Add CORS handling

--- app.js
+++ app.js
@@ -11,6 +11,19 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Set headers to prevent CORS errors
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  // Whenever a POST, PUT, etc. request is made, the browser will first send a preflight OPTIONS request 
+  // to see if it is allowed to in the first place...
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+})
+
 // API route handling
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
