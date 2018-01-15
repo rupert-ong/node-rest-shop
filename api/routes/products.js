164e85de00fa878e3e6630c45e6fb15d7829df45
rupert-ong <rupert.ong@gmail.com>
2018-01-14 20:55:54 -0500

    Modify POST and GET requests to save and fetch MongoDB document respectively

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
   res.status(200).json({
@@ -8,24 +10,43 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-  const product = {
+  const product = new Product({
+    _id: mongoose.Types.ObjectId(),
     name: req.body.name,
     price: req.body.price
-  };
-  res.status(201).json({
-    message: 'Handling POST requests to /products',
-    createdProduct: product
   });
+  product.save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: 'Handling POST requests to /products',
+        createdProduct: product
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 
 router.get('/:productId', (req, res, next) => {
   const id = req.params.productId;
-  const payload = id === 'special' ? {
-    message: 'Special ID discovered',
-    id: id
-  } : { message: `Handling GET request to /products/${id}` };
-
-  res.status(200).json(payload);
+  Product.findById(id)
+    .exec()
+    .then(doc => {
+      console.log(doc);
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res.status(404).json({
+          message: 'No valid entry found'
+        });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 
 router.patch('/:productId', (req, res, next) => {
