69b3267c2f940f2e8247e5a377463b4a4adefd72
rupert-ong <rupert.ong@gmail.com>
2018-01-14 21:12:24 -0500

    Modify DELETE request to delete MongoDB product document

--- api/routes/products.js
+++ api/routes/products.js
@@ -65,10 +65,18 @@ router.patch('/:productId', (req, res, next) => {
 
 router.delete('/:productId', (req, res, next) => {
   const id = req.params.productId;
-  res.status(200).json({
-    message: `Handling DELETE request to /products/${id}`
-  });
+  Product.remove({ _id: id })
+    .exec()
+    .then(result => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
 });
-
 
 module.exports = router;
