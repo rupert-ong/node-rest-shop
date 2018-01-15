acf06ee4d51e383e021309760a5d7b6eb731a44f
rupert-ong <rupert.ong@gmail.com>
2018-01-14 21:05:19 -0500

    Modify GET request to return all MongoDB product documents

--- api/routes/products.js
+++ api/routes/products.js
@@ -4,9 +4,16 @@ const mongoose = require('mongoose');
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Handling GET requests to /products'
-  });
+  Product.find()
+    .exec()
+    .then(docs => {
+      console.log('GET request: ' + docs);
+      res.status(200).json(docs);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 
 router.post('/', (req, res, next) => {
