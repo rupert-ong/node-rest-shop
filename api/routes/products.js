80e62d3f5c736d400801e82d3bda40c4754d6d03
rupert-ong <rupert.ong@gmail.com>
2018-01-28 12:19:09 -0500

    Add file filter, size limit and fix Windows filename issue for Multer

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,16 +2,27 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
+// Multer (Image Upload) Config
 const multer = require('multer');
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, './uploads/')
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + file.originalname);
+    cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
   }
 });
-const upload = multer({storage: storage});
+const fileFilter = (req, file, cb) => {
+  if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') cb(null, true);
+  else cb(null, false);
+}
+const upload = multer({ 
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: fileFilter
+});
 
 const Product = require('../models/product');
 const port = process.env.PORT || 3000;
