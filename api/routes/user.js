0ac5366df8dbddc2b5224bf63e1517a8a32f57d5
rupert-ong <rupert.ong@gmail.com>
2018-01-28 21:07:43 -0500

    Block new user signup when email exists

--- api/routes/user.js
+++ api/routes/user.js
@@ -6,25 +6,34 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 router.post('/signup', (req, res, next) => {
-  bcrypt.hash(req.body.password, 10, (err, hash) => {
-    if (err)  {
-      res.status(500).json({ error: err });
-    } else {
-      const user = new User({
-        _id: mongoose.Types.ObjectId(),
-        email: req.body.email,
-        password: hash
-      });
-      user.save()
-        .then(result => {
-          console.log(result);
-          res.status(201).json({
-            message: 'User created'
-          })
-        })
-        .catch(err => { console.log(err); res.status(500).json({ error: err }); });
-    }
-  });
+  User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+      if (user.length >= 1) {
+        return res.status(409).json({ message: 'Email exists ' });
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            res.status(500).json({ error: err });
+          } else {
+            const user = new User({
+              _id: mongoose.Types.ObjectId(),
+              email: req.body.email,
+              password: hash
+            });
+            user.save()
+              .then(result => {
+                console.log(result);
+                res.status(201).json({
+                  message: 'User created'
+                })
+              })
+              .catch(err => { console.log(err); res.status(500).json({ error: err }); });
+          }
+        });
+      }
+    })
+
 });
 
 router.post('/signout', (req, res, next) => {
