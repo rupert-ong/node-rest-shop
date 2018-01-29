3019c463597caf1b2518e4cd1e2f3b08a859e58d
rupert-ong <rupert.ong@gmail.com>
2018-01-28 21:23:51 -0500

    Add index optimization and validation to email for User schema

--- api/models/user.js
+++ api/models/user.js
@@ -4,7 +4,12 @@ const userSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   email: {
     type: String,
-    required: true
+    required: true,
+    // Not a validation. Just tells MongoDB this value should be unique
+    // which allows for indexing and searching optimization
+    unique: true,
+    // Email validation
+    match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
   },
   password: {
     type: String,
